0f2c23c33c7a19a305663e8487189059ff378a72
SebRouse <sjrouse@perse.co.uk>
2022-09-01 17:30:19 +0100

    kj

---
+++ DAWG.py
@@ -0,0 +1,55 @@
+class DawgNode:
+    NextID = 0
+    def __init__(self):
+        self.id  = DawgNode.NextID
+        DawgNode.NextID += 1
+        self.EndOfWord = False
+        self.children={}
+
+class Dawg:
+    def __init__(self):
+        self.root = DawgNode()
+        self.previousWord= ""
+        self.uncheckedNodes=[]
+        self.minimisedNodes={}
+    
+    def insert(self, word):
+
+        CurrentNode = self.root
+
+        LengthOfCommonPrefix = 0
+        length = min(len(word),len(self.previousWord))
+        for i in range(length):
+            if word[i] == self.previousWord[i]:
+                LengthOfCommonPrefix += 1
+            else:
+                break
+
+        if self.uncheckedNodes: 
+            CurrentNode = self.minimize(LengthOfCommonPrefix,CurrentNode)
+
+        for l in word[LengthOfCommonPrefix:]:
+            NextNode= DawgNode()
+            CurrentNode.children[l] = NextNode
+            self.uncheckedNodes.append( (CurrentNode,l,NextNode) )
+            CurrentNode =NextNode
+        
+        CurrentNode.EndOfWord=True
+        self.previousWord=word
+
+        
+
+
+        
+
+
+    def minimize(self, min, CurrentNode):
+        for i in range(len(self.uncheckedNodes)-1,min,-1):
+            parent, letter, child = self.uncheckedNodes.pop()
+            if child in self.minimisedNodes:
+                parent.children[letter]= self.minimisedNodes[child]
+            else:
+                self.minimisedNodes[child]= child
+            CurrentNode = parent
+        return CurrentNode
+            
