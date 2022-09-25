b46b8837f2db24058a3bf62a5e5bdeb548a06732
SebRouse <sjrouse@perse.co.uk>
2022-09-25 14:15:38 +0100

    couple bug fixes

--- Game.py
+++ Game.py
@@ -50,6 +50,8 @@ class Player:
 class Dictionary:
     def __init__(self):
         self._Dawg=None
+        self._languages[English()]
+
 
     def CreateDawg(self,filename):
         self._Dawg=Dawg()
@@ -81,18 +83,26 @@ class English:
         self._bag=[]
         for i in range(12):
             self._bag.append("E")
-        for i in range(9):
-            self._bag.append("I")
-            self._bag.append("A")
-        for i in range(8):
-            self._bag.append("O")
-        for i in range(6):
-            self._bag.append("N")
-            self._bag.append("R")
-            self._bag.append("T")
+            if i<9:
+                self._bag.append("I");self._bag.append("A");
+            if i<8:
+                self._bag.append("O")
+            if i<6:
+                self._bag.append("N");self._bag.append("R");self._bag.append("T")
+            if i <4:
+                self._bag.append("D");self._bag.append("L");self._bag.append("S");self._bag.append("U")
+            if i <3:
+                self._bag.append("G")
+            if i<2:
+                self._bag.append("B");self._bag.append("C");self._bag.append("F");self._bag.append("H");self._bag.append("M");self._bag.append("P");self._bag.append("V");self._bag.append("W");self._bag.append("Y");self._bag.append("blank")
+            if i<1:
+                self._bag.append("J");self._bag.append("K");self._bag.append("Q");self._bag.append("X");self._bag.append("Z")
+        self._dict = {"blank":0,"A":1,"E":1,"I":1,"L":1,"N":1,"O":1,"S":1,"T":1,"U":1,"D":2,"G":2,"B":3,"C":3,"M":3,"P":3,"F":4,"H":4,"V":4,"W":4,"Y":4,"K":5,"J":8,"X":8,"Q":10,"Z":10}
+
+
 
         self._pointsDict = {}
-        self._filename= "english.txt"
+        self._filename= "English.txt"
     
     def returnBag(self):
         return self._bag
