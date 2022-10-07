9555c636bffdcb20e8d1a5a63e192a8c65e28db5
SebRouse <sjrouse@perse.co.uk>
2022-10-07 09:26:11 +0100

    Spanish implementation complete - bar importing the dictionary coz it's blocked on school wifi

--- Languages.py
+++ Languages.py
@@ -55,7 +55,7 @@ class English:
                 self._bag.append("B");self._bag.append("C");self._bag.append("F");self._bag.append("H");self._bag.append("M");self._bag.append("P");self._bag.append("V");self._bag.append("W");self._bag.append("Y");self._bag.append("blank")
             if i<1:
                 self._bag.append("J");self._bag.append("K");self._bag.append("Q");self._bag.append("X");self._bag.append("Z")
-        self._dict = {"blank":0,"A":1,"E":1,"I":1,"L":1,"N":1,"O":1,"S":1,"T":1,"U":1,"D":2,"G":2,"B":3,"C":3,"M":3,"P":3,"F":4,"H":4,"V":4,"W":4,"Y":4,"K":5,"J":8,"X":8,"Q":10,"Z":10}
+        self._pointsDict = {"blank":0,"A":1,"E":1,"I":1,"L":1,"N":1,"O":1,"S":1,"T":1,"U":1,"D":2,"G":2,"B":3,"C":3,"M":3,"P":3,"F":4,"H":4,"V":4,"W":4,"Y":4,"K":5,"J":8,"X":8,"Q":10,"Z":10}
 
         self._filename= "English.txt"
     
@@ -87,6 +87,8 @@ class Spanish:
             if i<1:
                 self._bag.append("CH");self._bag.append("F");self._bag.append("J");self._bag.append("LL");self._bag.append("Ñ");self._bag.append("Q")
                 self._bag.append("RR");self._bag.append("V");self._bag.append("X");self._bag.append("Y");self._bag.append("Z")
+
+        self._pointsDict = {"blank":0,"Z":10,"Y":4,"X":8,"V":4,"U":1,"T":1,"S":1,"RR":8,"R":1,"Q":5,"P":3,"O":1,"Ñ":8,"N":1,"M":3,"LL":8,"L":1,"J":8,"I":1,"H":4,"G":2,"F":4,"E":1,"D":2,"CH":5,"C":3,"B":3,"A":1}
 
         self._filename="Spanish.txt"
 
