4792c515152e22b2e69b153374e059bad58244c1
SebRouse <sjrouse@perse.co.uk>
2022-12-03 11:16:40 +0000

    small changes to database code

--- databse.py
+++ databse.py
@@ -7,26 +7,21 @@ class Databse():
    cur = con.cursor()
    res = cur.execute("""CREATE TABLE IF NOT EXISTS Players (
       Username VARCHAR(255) PRIMARY KEY NOT NULL,
-      PasswordHash VARCHAR(255) NOT NULL,
+      Password VARCHAR(255) NOT NULL,
+      NumGames INT,
       Wins INT,
-      Draws INT,
       Losses INT,
-      HighestScoringWord VARCHAR(255)
-      HighestScore INT
    );""")
    res = cur.execute("""CREATE TABLE IF NOT EXISTS Games(
       GameID INT PRIMARY KEY,
-      NumPlayers INT,
-      NumHumanPlayers INT,
-      NumAIPlayers INT,
       TurnNo INT,
-      AIDifficulty VARCHAR(255),
       Board VARCHAR(255),
       Scores VARCHAR(255),
-      Ended BOOLEAN,
+      Bag VARCHAR(255),
       Language VARCHAR(255)
       );""")
    res= cur.executemany("""CREATE TABLE IF NOT EXISTS GamesPlayed(
+      Id INT PRIMARY KEY,
       Username VARCHAR(255),
       GameID INT
       );""")
